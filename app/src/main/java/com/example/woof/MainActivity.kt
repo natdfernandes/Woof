f5f56ffc2dfe1c33d80f3240b305e1a760b9d62b
nataliafernandes <nataliadiasfernande@gmail.com>
2026-07-02 10:33:15 -0300

    Woof adding an if statement to display the elements

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -163,15 +163,6 @@ fun DogItem(
                     )
                 )
             }
-            DogHobby(
-                dog.hobbies,
-                modifier = Modifier.padding(
-                    start = dimensionResource(R.dimen.padding_medium),
-                    top = dimensionResource(R.dimen.padding_small),
-                    end = dimensionResource(R.dimen.padding_medium),
-                    bottom = dimensionResource(R.dimen.padding_medium)
-                )
-            )
         }
 
     }
